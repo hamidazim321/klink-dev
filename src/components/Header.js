e1e78756d67dda45cfb2068d55b2f5c41216ec5f
Hamid Azim <hamidazim321@gmail.com>
2023-11-19 04:58:24 +0500

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,48 @@
 import React from "react";
+import { signOut } from "firebase/auth";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Stack, Image } from "react-bootstrap";
+import { Stack, Image, Button, Dropdown } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
 import logo from "../assets/logo.svg";
-export default function Header({name}) {
+import { useDispatch } from "react-redux";
+import { auth } from "../firebase";
+import { setUser } from "../redux/CurrentUser/currentUser";
+export default function Header({ name }) {
+  const dispatch = useDispatch()
+  const handleLogout = async() => {
+    try {
+      await signOut(auth)
+      dispatch(setUser(null))
+      console.log('logged out')
+    } catch (err) {
+      console.error(err)
+    }
+  }
   return (
-    <Stack direction="horizontal" className="w-100" gap={3}>
-      <div className="w-50 d-flex justify-content-between">
-        <div className="p-2 fw-bold">{name}</div>
-        <div className="p-2">
-          <Image className="w-50" src={logo} />
+    <Stack direction="horizontal" className="w-100 sticky-top" gap={3}>
+      <div className="w-100 d-flex justify-content-between align-items-baseline">
+        <div className="p-2 gap-2 fw-bold d-flex align-items-center">
+          <Dropdown >
+            <Dropdown.Toggle
+              variant="outline"
+              id="dropdown-basic"
+              className="fs-1 p-2"
+            >
+              <FaUserCircle />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Button type="button" variant="outline" onClick={handleLogout}>
+                  Logout
+                </Button>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <p className="fs-4 align-self-end">{name}</p>
+        </div>
+        <div className="p-2 align-self-center ms-auto">
+          <Image className="w-75" src={logo} />
         </div>
       </div>
     </Stack>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,8 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { signOut } from 'firebase/auth'
-import { setUser } from '../redux/CurrentUser/currentUser'
-import { auth } from '../firebase'
-
 export default function Home() {
-  const dispatch = useDispatch()
-  const handleLogOut = async() => {
-    try {
-      await signOut(auth)
-      dispatch(setUser(null))
-      console.log('logged out')
-    } catch (err) {
-      console.error(err)
-    }
-  }
   return (
     <div>
-      <button type='button' onClick={handleLogOut}>Logout</button>
+      <button type='button'>Home</button>
     </div>
   )
 }
