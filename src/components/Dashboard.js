58fb4c7e616436c7b501435b1839388b58ac51d6
Hamid Azim <hamidazim321@gmail.com>
2023-11-25 14:43:08 +0500

    add bootstrap grid: Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Button, Container, Card, Image } from "react-bootstrap";
+import { Button, Container, Card, Image, Col, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../assets/logo.svg";
 import { useNavigate } from "react-router-dom";
@@ -9,45 +9,84 @@ export default function Dashboard() {
   const navigate = useNavigate();
   const { username } = useSelector((state) => state.currentUser);
   useEffect(() => {
-    if (username){
-      navigate('/Home')
+    if (username) {
+      navigate("/Home");
     }
-  }, [username, navigate])
+  }, [username, navigate]);
   return (
-    <div>
-      <Container className="w-100 p-4 d-flex flex-column gap-4 pt-5">
-        <div>
+    // <div>
+    //   <Container className="w-100 p-4 d-flex flex-column gap-4 pt-5">
+    //     <div>
+    // <Image className="w-25" src={logo} />
+    //     </div>
+    //     <div>
+    //       <h1 className="h1 display-1 fw-bold">Happening</h1>
+    //       <h2 className=" h2 display-2 fw-bold">now</h2>
+    //     </div>
+    //     <Card className="w-100 w-sm-100 border-0">
+    //       <Card.Body className="d-flex flex-column">
+    //         <Card.Title className="fs-1 mb-4">Join Now</Card.Title>
+    // <Button
+    //   variant="primary"
+    //   className="rounded-pill w-50 fs-5 fw-semibold mb-3"
+    //   type="button"
+    //   onClick={() => navigate('/SignUp')}
+    // >
+    //   Create account
+    // </Button>
+    //         <Card.Text className="mb-1 fs-5 ">
+    //           Already have an account?
+    //         </Card.Text>
+    //         <Button
+    //           variant="outline"
+    //           className="border border-secondary-subtle rounded-pill w-50 fs-5 fw-semibold"
+    //           type="button"
+    //           onClick={() => navigate('/Login')}
+    //         >
+    //           Sign in
+    //         </Button>
+    //       </Card.Body>
+    //     </Card>
+    //   </Container>
+    // </div>
+    <Container fluid className="p-2 p-md-5">
+      <Row className="gy-2">
+        <Col className="col-12 col-lg-6">
           <Image className="w-25" src={logo} />
-        </div>
-        <div>
+        </Col>
+        <Col>
           <h1 className="h1 display-1 fw-bold">Happening</h1>
           <h2 className=" h2 display-2 fw-bold">now</h2>
-        </div>
-        <Card className="w-100 w-sm-100 border-0">
-          <Card.Body className="d-flex flex-column">
-            <Card.Title className="fs-1 mb-4">Join Now</Card.Title>
-            <Button
-              variant="primary"
-              className="rounded-pill w-50 fs-5 fw-semibold mb-3"
-              type="button"
-              onClick={() => navigate('/SignUp')}
-            >
-              Create account
-            </Button>
-            <Card.Text className="mb-1 fs-5 ">
-              Already have an account?
-            </Card.Text>
-            <Button
-              variant="outline"
-              className="border border-secondary-subtle rounded-pill w-50 fs-5 fw-semibold"
-              type="button"
-              onClick={() => navigate('/Login')}
-            >
-              Sign in
-            </Button>
-          </Card.Body>
-        </Card>
-      </Container>
-    </div>
+        </Col>
+      </Row>
+      <Row className="gy-2">
+        <Col className="col-12">
+          <p className="display-6 fw-bold">Join Now</p>
+        </Col>
+        <Col className="col-12">
+          <Button
+            variant="primary"
+            className="rounded-pill fs-5 px-5 fw-semibold mb-3"
+            type="button"
+            onClick={() => navigate("/SignUp")}
+          >
+            Create account
+          </Button>
+        </Col>
+        <Col className="col-12">
+          <span>Already have an account?</span>
+        </Col>
+        <Col className="col-12">
+          <Button
+            variant="outline"
+            className="border px-5 border-secondary-subtle rounded-pill fs-5 fw-semibold"
+            type="button"
+            onClick={() => navigate("/Login")}
+          >
+            Sign in
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
