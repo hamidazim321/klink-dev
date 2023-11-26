cce8e45c3ff81cce9acd83a7439d2241d0d0e6fa
Hamid Azim <hamidazim321@gmail.com>
2023-11-26 19:44:22 +0500

    Set sidebar to the left

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -23,11 +23,11 @@ export default function SideBar({closeBar, open}) {
   return (
     <Container 
     style={{display: open ? "block" : "none"}}
-    className="border border-2 width-75 p-3 width-s-50 width-md-25 width-lg-25 position-fixed z-3 bg-white left-0 top-0 inLeft"
+    className="border border-2 width-75 p-3 width-s-50 width-md-25 width-lg-25 position-fixed z-3 bg-white end-0 top-0 inRight"
     >
       <div className="d-flex justify-content-between align-items-center border-bottom py-3">
         <FaUserCircle className="fs-1" />
-        <CloseButton className="fs-3" onClick={closeBar} />
+        <CloseButton className="fs-4" onClick={closeBar} />
       </div>
       <Stack>
         <span className="p-2" onClick={() => navigate('/post')}>Post</span>
